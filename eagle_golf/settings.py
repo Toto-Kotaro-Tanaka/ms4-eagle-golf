7899d40c8135a803aa8c39e6c0f59d224fb18130
Toto-Kotaro-Tanaka <72462399+Toto-Kotaro-Tanaka@users.noreply.github.com>
2021-07-10 13:05:28 +0000

    Remove secret key and update debug mode on settings.py

--- eagle_golf/settings.py
+++ eagle_golf/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uuxx+_p7ev_ows+=xvg1u&@^gt68z-_eh&^@0b9)()%z&(pg-!'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['ms4-eagle-golf.herokuapp.com', 'localhost']
 
