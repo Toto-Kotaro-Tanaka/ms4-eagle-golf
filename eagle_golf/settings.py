4ddfc5bf5764e5358dba98bf510ae0e8523a01ae
Toto-Kotaro-Tanaka <ktntrtmt@gmail.com>
2021-07-15 00:27:50 +0000

    Put back database code on settings.py as issue is solved

--- eagle_golf/settings.py
+++ eagle_golf/settings.py
@@ -120,16 +120,17 @@ WSGI_APPLICATION = 'eagle_golf.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-}
-
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': BASE_DIR / 'db.sqlite3',
-#         }
-#     }
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
