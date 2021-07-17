8a8a8cac865d7008cb9387461f5270799f363fdb
Toto-Kotaro-Tanaka <72462399+Toto-Kotaro-Tanaka@users.noreply.github.com>
2021-07-17 13:30:59 +0000

    Add 404 page

--- eagle_golf/settings.py
+++ eagle_golf/settings.py
@@ -26,6 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = False
 
 ALLOWED_HOSTS = ['ms4-eagle-golf.herokuapp.com', 'localhost']
 
